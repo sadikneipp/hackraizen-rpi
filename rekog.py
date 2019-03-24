833155bb69da31773aef30b63a054239ed00a0ac
sadikneipp <sadi.neto13@gmail.com>
2019-03-24 06:45:28 -0300

    looping rekognition code

--- rekog.py
+++ rekog.py
@@ -1,29 +1,38 @@
 import boto3
+import requests
+from acquisition import camera
+import time 
 
-if __name__ == "__main__":
+bucket='rekognition-raizen'
+photo='image.jpg'
+s3 = boto3.resource('s3')
+WAIT_TIME = 10
 
-    bucket='rekognition-raizen'
-    photo='image.jpg'
+def tell_db(payload):
+    return
+
+def is_plate(text):
+    if len(text) == 8 and text[3] == '-':
+        return True
     
-    s3 = boto3.resource('s3')
+    return False
+
+while True:
+    webcam = camera.init_camera()
+    for i in range(2):
+        camera.save_ss(webcam, filename)
+        time.sleep(1)
+    camera.stop_camera(webcam)
+
     data = open(photo, 'rb')
     s3.Bucket(bucket).put_object(Key=photo, Body=data)
 
     client=boto3.client('rekognition', region_name='us-east-1')
 
-  
-    response=client.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
-
-                        
+    response=client.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':photo}})    
     textDetections=response['TextDetections']
-    print response
-    print 'Detected text'
     for text in textDetections:
-            print 'Detected text:' + text['DetectedText']
-            print 'Confidence: ' + "{:.2f}".format(text['Confidence']) + "%"
-            print 'Id: {}'.format(text['Id'])
-            if 'ParentId' in text:
-                print 'Parent Id: {}'.format(text['ParentId'])
-            print 'Type:' + text['Type']
-            print
+        if text['Confidence'] > 0.9 and is_plate(text['DetectedText']):
+            tell_db(text['DetectedText'])
 
+    time.sleep(WAIT_TIME)
