843050f8cf106a12568a111c10a8efc6945c9cf8
sadikneipp <sadi.neto13@gmail.com>
2019-03-23 21:58:27 -0300

    fixes leds not turning off

--- blink.py
+++ blink.py
@@ -6,7 +6,8 @@ set_brightness(0.1)
 c_to_rgb = {
     'r':   (255, 0, 0),
     'g':  (0, 255, 0),
-    'b': (0, 0, 255)
+    'b': (0, 0, 255),
+    '0': (0, 0, 0)
 }
 
 def set_color(color, timeout=2):
@@ -17,6 +18,10 @@ def set_color(color, timeout=2):
     show()
     time.sleep(timeout)
     set_brightness(0)
-
+    clear()
+    code = c_to_rgb['0']
+    for i in range(8):
+        set_pixel(i, code[0], code[1], code[2])
+        
 if __name__ == '__main__':
     set_color('r')
