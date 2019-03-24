0281709b34826967f7b0b48c803048628d8ac060
sadikneipp <sadi.neto13@gmail.com>
2019-03-24 09:36:20 +0000

    plate detector sync

--- plate_detector.py
+++ plate_detector.py
@@ -1,15 +1,23 @@
 import subprocess
 from acquisition import camera
+import os
+import time
 
+pwd = os.getcwd()
 filename = 'image.jpg'
+while True:
+    webcam = camera.init_camera()
+    for i in range(2):
+        camera.save_ss(webcam, filename)
+        time.sleep(1)
+    camera.stop_camera(webcam)
 
-webcam = camera.init_camera()
-camera.save_ss(webcam, 'image.jpg')
-
-test = subprocess.Popen(["sudo", "docker", "run", "-it", "--rm" ,
-                         "-v" , "$(pwd):/data:ro", "openalpr", 
-                         "-c" ,"eu" , filename],
+    test = subprocess.Popen(["sudo", "docker", "run", "-it", "--rm" ,
+                         "-v" , pwd + ":/data:ro", "openalpr", 
+                         "-c" ,"eu" , "-p", "@@@####", filename],
                         stdout=subprocess.PIPE)
 
-output = test.communicate()[0]
-print(output)
+    #os.remove(filename)
+
+    output = test.communicate()[0]
+    print(output)
