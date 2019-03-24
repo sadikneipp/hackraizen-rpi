fca6995356fb1ab23b43bc3025e1c439fe88c105
sadikneipp <sadi.neto13@gmail.com>
2019-03-23 22:04:44 -0300

    fixed finally all leds

--- blink.py
+++ blink.py
@@ -6,22 +6,18 @@ set_brightness(0.1)
 c_to_rgb = {
     'r':   (255, 0, 0),
     'g':  (0, 255, 0),
-    'b': (0, 0, 255),
-    '0': (0, 0, 0)
+    'b': (0, 0, 255)
 }
 
 def set_color(color, timeout=2):
-    set_brightness(0.1)
+    set_brightness(1)
     code = c_to_rgb[color]
     for i in range(8):
         set_pixel(i, code[0], code[1], code[2])
     show()
     time.sleep(timeout)
-    set_brightness(0)
     clear()
-    code = c_to_rgb['0']
-    for i in range(8):
-        set_pixel(i, code[0], code[1], code[2])
-        
+    show()
+
 if __name__ == '__main__':
     set_color('r')
